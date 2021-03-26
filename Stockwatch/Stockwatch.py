a8a89accb9da7ffb0bf1669df88df4267a73fde1
root <marvinnitz18@gmail.com>
2021-03-26 12:50:56 +0100

    add colors

--- Stockwatch/Stockwatch.py
+++ Stockwatch/Stockwatch.py
@@ -9,7 +9,7 @@ api_url = "http://api.marketstack.com/v1/intraday/latest"
 
 stocks = open("stocks.txt","r")
 stocks = stocks.read()
-print(stocks)
+print("Stocks to watch: "+stocks)
 
 
 #Initiaziled
@@ -18,6 +18,9 @@ print("API REQUEST: "+api_url +"?access_key="+access_key+"&symbols="+stocks)
 
 #API Request
 r = requests.get(api_url +"?access_key="+access_key+"&symbols="+stocks)
+if (r.status_code != 200):
+    print("API Request failed !")
+    exit()
 
 #Stocks into list
 stocks = stocks[:-2]
@@ -32,7 +35,13 @@ data = data['data']
 count = 0
 for i in range(length):
     opening_price = data[count]['open']
-    print(stocks[count]+" "+str(opening_price))
+    last_price = data[count]['last']
+    
+    print(stocks[count]+' opening '+'\033[31m'+str(opening_price)+'\033[0m')
+    print(stocks[count]+' last '+str(last_price))
+    
+    
     count = count +1
+    print("\n")
 
 
