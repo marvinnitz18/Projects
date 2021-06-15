747193b295cf28deaf73c6be683c3fc3536f56d6
Marvin Nitz <marvin.nitz18@gmail.com>
2021-06-15 22:33:19 +0200

    recylce ping

--- latency-monitor/lartency-monitor.py
+++ latency-monitor/lartency-monitor.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 
-import os
-import subprocess as sp
-
+from pythonping import ping
 
 
 def get_latency(host):
-    param = '-qc1'
-    command = ['ping', param, host]
-    print(sp.call(command))
+   ping(host, verbose=True) 
 
 
 get_latency("1.1.1.1")
