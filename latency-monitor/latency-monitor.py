2109cae1ecfb36623b66b775faa0e1f2e784266b
Marvin Nitz <marvin.nitz18@gmail.com>
2021-06-16 19:03:35 +0200

    add matplot

--- latency-monitor/latency-monitor.py
+++ latency-monitor/latency-monitor.py
@@ -3,35 +3,30 @@
 from pythonping import ping
 import matplotlib.pyplot as plt
 
-x = [1,2,3]
-# corresponding y axis values
-y = [2,4,1]
+x = []
+y = []
+i = 0
   
-# plotting the points 
+def get_latency(host):
+   result = str(ping(host,count=1))
+   result = result.split(" ")
+   result = result[6]
+   result = result[:-9]
+   x.append(str(result))
+   return result
+
+
+
+for x in range(5):
+   get_latency("1.1.1.1")
+   y.append(str(i))
+   i = i + 1
+
 plt.plot(x, y)
   
-# naming the x axis
+
 plt.xlabel('x - axis')
-# naming the y axis
 plt.ylabel('y - axis')
   
-# giving a title to my graph
 plt.title('My first graph!')
-  
-# function to show the plot
 plt.show()
-
-
-def get_latency(host):
-   try:
-      result = str(ping(host,count=1))
-      result = result.split(" ")
-      result = result[6]
-      result = result[:-9]
-      return result
-   except:
-      print("Ping failed")
-
-
-
-print(get_latency("1.1.1.1"))
